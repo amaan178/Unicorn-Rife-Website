ee8afc4661ac7db81654082f8cffcbc28519ae50
Amaan Jambura <amaanjambura786@gmail.com>
2020-12-20 15:05:25 +0530

    Intial Commit!

---
+++ app/assets/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * scripts.js
+ * Contains Script for basic static website named "White Graphics"
+ */
+
+$(window).on('load', function(){
+    $("#preloader").delay(5000).fadeOut('slow'); 
+});
